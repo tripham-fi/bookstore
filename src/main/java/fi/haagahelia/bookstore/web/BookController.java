e6077d02ea63bfd02fc80a712b2f8031b66e0407
tripham <tri.pham@myy.haaga-helia.fi>
2026-02-05 00:40:17 +0200

    2. Bookstore: Listpage

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -1,16 +1,33 @@
 package fi.haagahelia.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.haagahelia.bookstore.repository.BookRepository;
+
 
 
 @Controller
 public class BookController {
 
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @RequestMapping(value = "/", method=RequestMethod.GET)
     public String index() {
         return "index";
     }
+
+    @RequestMapping(value = "/booklist", method=RequestMethod.GET)
+    public String bookList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
+    }
+    
     
 }
