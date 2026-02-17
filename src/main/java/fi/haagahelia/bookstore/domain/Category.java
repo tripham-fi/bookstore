18e65c0944511cf64032a1a60f2c0ba6abcd68d5
tripham <tri.pham@myy.haaga-helia.fi>
2026-02-17 19:16:58 +0200

    update repository and model

--- src/main/java/fi/haagahelia/bookstore/domain/Category.java
+++ src/main/java/fi/haagahelia/bookstore/domain/Category.java
@@ -2,6 +2,8 @@ package fi.haagahelia.bookstore.domain;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -19,6 +21,7 @@ public class Category {
     private String name;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    @JsonIgnore
 	private List<Book> books;
 
     public Category() {
--- src/main/java/fi/haagahelia/bookstore/repository/BookRepository.java
+++ src/main/java/fi/haagahelia/bookstore/repository/BookRepository.java
@@ -1,8 +1,13 @@
 package fi.haagahelia.bookstore.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fi.haagahelia.bookstore.domain.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long>{
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    List<Book> findByAuthorContainingIgnoreCase(String author);
 }
